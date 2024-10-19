50a152e05aca3f5ec0050b6cad13717b7b7270c1
Yogeeswaran03 <yogees97@gmail.com>
2024-10-19 23:25:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [advice,setAdvice]=useState("Please Click the button to get Advice");
+   
+  async function getadvice(){
+    const response=await fetch
+    ("https://api.adviceslip.com/advice");
+    const data=await response.json();
+    setAdvice(data.slip.advice);
+  }
+
+  useEffect(function(){
+    getadvice();
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>"{advice}"</h1>
+      <button onClick={getadvice}>Get Advice</button>
+      <h3><a href='https://yogeeswaran-m.vercel.app/'>Developed by Yogeeswaran M</a> </h3>
     </div>
   );
 }
